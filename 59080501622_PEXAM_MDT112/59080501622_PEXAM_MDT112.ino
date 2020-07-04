17d6eafecc58ada2047d7f7f17d69cc298bee7a9
job.ljee555@gmail.com <job.ljee555@gmail.com>
2020-07-04 15:00:09 +0700

    7. complete

--- 59080501622_PEXAM_MDT112/59080501622_PEXAM_MDT112.ino
+++ 59080501622_PEXAM_MDT112/59080501622_PEXAM_MDT112.ino
@@ -16,6 +16,9 @@ Adafruit_SSD1306 oled = Adafruit_SSD1306(128, 32, &Wire);
 
 int Time = 1;
 long lasttimeStrateChange = 0;
+bool laststate = true;
+int Timetopush,TimeAWS7 = 0;
+
 void setup(){
     pinMode(2,INPUT_PULLUP);
     pinMode(8, OUTPUT);
@@ -23,6 +26,7 @@ void setup(){
     stepper.connectToPins(MOTOR_IN1_PIN, MOTOR_IN2_PIN, MOTOR_IN3_PIN, MOTOR_IN4_PIN);
 }
 int MILLTIME =0;
+
 void loop(){
     long cuurrentTime = millis();
 if (Time == 1)
@@ -38,6 +42,7 @@ if (Time == 1)
     {
     stepper.moveRelativeInSteps(512);
     Serial.println("Step CW 90  Degrees");
+    lasttimeStrateChange = cuurrentTime;
     MILLTIME =1;
     }//3
 
@@ -45,6 +50,7 @@ if (Time == 1)
     {
     stepper.moveRelativeInSteps(-1024);
     Serial.println("Step CCW 180  Degrees");
+    lasttimeStrateChange = cuurrentTime;
     MILLTIME =2;
     }//4
 
@@ -55,23 +61,96 @@ if (Time == 1)
     oled.setTextSize(1);
     oled.print("Hello MDT!!");
     oled.display();
+    lasttimeStrateChange = cuurrentTime;
     MILLTIME=3;
     }//5
 
     if (MILLTIME == 3 && cuurrentTime - lasttimeStrateChange >= 10) {
     oled.clearDisplay();
     oled.display();
+    lasttimeStrateChange = cuurrentTime;
     MILLTIME=4;
     }//5
 
     if (MILLTIME == 4 && cuurrentTime - lasttimeStrateChange >= 350) {
     tone(8,500,200);
+    lasttimeStrateChange = cuurrentTime;
     MILLTIME=5;
     Time =2;
     }//6
 
 }
-if (Time == 2 ) {
+if (Time == 2 ) 
+{
+    bool currentSteate = digitalRead(2);
+    if(laststate > currentSteate)
+    {
+        Timetopush++;
+        TimeAWS7++;
+    }
+    laststate = currentSteate;
+    delay(50);
+
+    if(Timetopush == 1 && TimeAWS7 == 1)
+    {
+        stepper.setSpeedInStepsPerSecond(256);
+        stepper.setAccelerationInStepsPerSecondPerSecond(512);
+        stepper.moveRelativeInSteps(512);
+
+        TimeAWS7 =0;
+    }
+    if(Timetopush == 2 && TimeAWS7 == 1)
+    {
+        stepper.setSpeedInStepsPerSecond(256);
+        stepper.setAccelerationInStepsPerSecondPerSecond(512);
+        stepper.moveRelativeInSteps(512);
+
+        TimeAWS7 =0;
+    }
+    if(Timetopush == 3 && TimeAWS7 == 1)
+    {
+        stepper.setSpeedInStepsPerSecond(256);
+        stepper.setAccelerationInStepsPerSecondPerSecond(512);
+        stepper.moveRelativeInSteps(512);
+
+        TimeAWS7 =0;
+    }
+    if(Timetopush == 4 && TimeAWS7 == 1)
+    {
+        stepper.setSpeedInStepsPerSecond(256);
+        stepper.setAccelerationInStepsPerSecondPerSecond(512);
+        stepper.moveRelativeInSteps(512);
+
+        TimeAWS7 =0;
+    }
+    if(Timetopush == 5 && TimeAWS7 == 1)
+    {
+        stepper.setSpeedInStepsPerSecond(256);
+        stepper.setAccelerationInStepsPerSecondPerSecond(512);
+        stepper.moveRelativeInSteps(512);
+
+        TimeAWS7 =0;
+    }
+    if(Timetopush == 6 && TimeAWS7 == 1)
+    {
+        stepper.setSpeedInStepsPerSecond(256);
+        stepper.setAccelerationInStepsPerSecondPerSecond(512);
+        stepper.moveRelativeInSteps(512);
+
+        TimeAWS7 =0;
+    }
+    if(Timetopush == 7 && TimeAWS7 == 1)
+    {
+        stepper.setSpeedInStepsPerSecond(256);
+        stepper.setAccelerationInStepsPerSecondPerSecond(512);
+        stepper.moveRelativeInSteps(512);
+
+        TimeAWS7 =0;
+    }
+    
+
+    
+
     
 }
 
